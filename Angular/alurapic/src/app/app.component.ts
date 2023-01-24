fdb0df684eb2d9654bc7d2b4c4d0c5d90018d69b
rmcedo <rafael9408@gmail.com>
2023-01-24 10:10:16 -0300

    Continuação do Curso de Angular

--- Angular/alurapic/src/app/app.component.ts
+++ Angular/alurapic/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  photos = [
-    {
-      url: 'https://m.extra.globo.com/incoming/18470804-54b-64a/w367h550-PROP/cachorro-ri.jpg',
-      description: 'Cachorro Sorrindo'
-    },
-    {
-      url: 'https://veja.abril.com.br/wp-content/uploads/2016/12/cc3a3o.png?w=1024&resize=1200,800',
-      description: 'Cachorro Sorring'
-    }
-  ];
 }
--- Angular/alurapic/src/app/app.module.ts
+++ Angular/alurapic/src/app/app.module.ts
@@ -1,21 +1,20 @@
-
-import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app.routing.module';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+
 import { PhotosModule } from './photos/photos.module';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    PhotosModule,
     AppRoutingModule,
-    PhotosModule
-
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
