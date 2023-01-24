242f85deecbe9e9d2e7e38e421995f2ca50c50c6
rmcedo <rafael9408@gmail.com>
2023-01-24 17:13:36 -0300

    Continuação dos Estudos de Angular

--- Angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ Angular/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,25 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
+import { PhotoService } from './../photo/photo.service';
 import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Photo } from './../photo/photo';
 
 @Component({
-  selector: 'app-photo-list',
+  selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
 export class PhotoListComponent implements OnInit {
 
-  photos: Object[] = [];
+  photos: Photo[] = []
+  filter: string = ''
+  hasMore: boolean = true
+  currentPage: number = 1
+  userName: string = ''
 
-  constructor(private photoService: PhotoService, private activatedRoute: ActivatedRoute) {
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+    ) { }
 
-  }
   ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.params.userName
-    this.photoService
-      .listFromUser(userName)
-      .subscribe(photos =>
-        this.photos = photos
-      )
+    this.userName= this.activatedRoute.snapshot.params.userName
+    this.photos = this.activatedRoute.snapshot.data.photos
+
+
+
   }
+
+
+  load(){
+    this.photoService.listFromUserPaginated(this.userName, ++this.currentPage).subscribe(photos=>
+      {
+        this.photos= this.photos.concat(photos)
+        if(!photos.length) this.hasMore=false
+
+      })
+  }
+
 }
--- Angular/alurapic/src/app/photos/photo/photo.service.ts
+++ Angular/alurapic/src/app/photos/photo/photo.service.ts
@@ -1,10 +1,10 @@
 import { Photo } from './photo';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 const API = 'http://localhost:3000'
 
-@Injectable({ providedIn: 'root'} )
+@Injectable({ providedIn: 'root' })
 export class PhotoService {
 
 
@@ -12,9 +12,14 @@ export class PhotoService {
 
   listFromUser(userName: string) {
 
-    return this.http.get<Photo[]>(API + '/'+ userName + '/photos')
+    return this.http.get<Photo[]>(API + '/' + userName + '/photos')
+  }
+  listFromUserPaginated(userName: string, page: number) {
 
+    const params = new HttpParams()
+      .append('page', page.toString())
 
+    return this.http.get<Photo[]>(API + '/' + userName + '/photos',{ params })
   }
 
 }
